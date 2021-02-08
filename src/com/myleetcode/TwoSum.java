1581adc3d2942a6c8b975f6ff50fcc6d2312b467
邱勇峰 <qiuyongfeng@miaozhen.com>
2021-02-08 18:21:11 +0800

    first commit

---
+++ src/com/myleetcode/TwoSum.java
@@ -0,0 +1,20 @@
+package com.myleetcode;
+
+/**
+ * @author :qiuyongfeng
+ * @description :
+ * @date :2021-02-08
+ **/
+public class TwoSum {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0;i < nums.length;i++){
+
+        }
+        return null;
+    }
+}
