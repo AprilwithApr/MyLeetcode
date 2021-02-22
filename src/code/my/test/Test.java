150295e6ea6e564a54c41e72b59c276430943a3c
邱勇峰 <qiuyongfeng@miaozhen.com>
2021-02-22 18:45:37 +0800

    fix

--- src/code/my/test/Test.java
+++ src/code/my/test/Test.java
@@ -1,14 +1,25 @@
 package code.my.test;
 
 
+import java.util.Stack;
 
 public class Test {
 
     public static void main(String[] args) {
-        int a = new Integer(1);
-        System.out.println(a);
+
+        Stack<String>[] stacks = new Stack[2];
+
+        Stack<String> s1 = new Stack<>();
+        Stack<String> s2 = new Stack<>();
+
+        s1.push("aaa");
+        s1.push("1");
+        stacks[0] = s1;
+        stacks[1] = s2;
+
     }
 }
+
 
 
 
