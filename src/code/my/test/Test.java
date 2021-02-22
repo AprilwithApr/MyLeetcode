9f84be3d0c9d5c58bad66e4b5bab2ec4b045cca2
邱勇峰 <qiuyongfeng@miaozhen.com>
2021-02-22 18:47:28 +0800

    update

--- src/code/my/test/Test.java
+++ src/code/my/test/Test.java
@@ -1,21 +1,11 @@
 package code.my.test;
 
 
-import java.util.Stack;
-
 public class Test {
 
     public static void main(String[] args) {
 
-        Stack<String>[] stacks = new Stack[2];
-
-        Stack<String> s1 = new Stack<>();
-        Stack<String> s2 = new Stack<>();
-
-        s1.push("aaa");
-        s1.push("1");
-        stacks[0] = s1;
-        stacks[1] = s2;
+        System.out.println("111");
 
     }
 }
