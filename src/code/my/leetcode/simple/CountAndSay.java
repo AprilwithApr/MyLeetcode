78d7720528ed1798eacdcbdb388216d3ca99db6f
邱勇峰 <qiuyongfeng@miaozhen.com>
2021-02-25 18:50:38 +0800

    new file

---
+++ src/code/my/leetcode/simple/CountAndSay.java
@@ -0,0 +1,58 @@
+package code.my.leetcode.simple;
+
+/**
+ * @author :qiuyongfeng
+ * @description :
+ * @date :2021-02-25
+ * <p>
+ * 给定一个正整数 n ，输出外观数列的第 n 项。
+ * <p>
+ * 「外观数列」是一个整数序列，从数字 1 开始，序列中的每一项都是对前一项的描述。
+ * <p>
+ * 你可以将其视作是由递归公式定义的数字字符串序列：
+ * <p>
+ * countAndSay(1) = "1"
+ * countAndSay(n) 是对 countAndSay(n-1) 的描述，然后转换成另一个数字字符串。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/count-and-say
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ **/
+public class CountAndSay {
+    public static void main(String[] args) {
+        System.out.println(countAndSay(3));
+    }
+
+    public static String countAndSay(int n) {
+
+        if (n == 1) return "1";
+
+        String s = countAndSay(n -1);
+        String res = "";
+        int p = 0;
+        int k = 0;
+        while (p < s.length()){
+            if (s.charAt(p) == s.charAt(k)){
+                k++;
+                if (k == s.length()){
+                    res += (k - p) + "" + s.charAt(k);
+                    break;
+                }
+            }else{
+                res += (k - p) + "" + s.charAt(k);
+                k++;
+                p = k;
+            }
+        }
+        return res;
+    }
+}
+//1
+//11
+//21
+//1211
+//111221
+//312211
+//13122221
+//1113114211
+//311321141221
--- src/code/my/leetcode/simple/SearchInsertPosition.java
+++ src/code/my/leetcode/simple/SearchInsertPosition.java
@@ -2,15 +2,35 @@ package code.my.leetcode.simple;
 
 /**
  * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
- *
+ * <p>
  * 你可以假设数组中无重复元素。
  */
 public class SearchInsertPosition {
     public static void main(String[] args) {
-        System.out.println(searchInsert(new int[]{1,2,3,4,5},3));
+        System.out.println(searchInsert(new int[]{1, 3, 5, 6}, 5));
     }
 
     public static int searchInsert(int[] nums, int target) {
-        return 1;
+
+        if (nums.length == 0 || target < nums[0]) return 0;
+        if (target > nums[nums.length - 1]) return nums.length;
+
+        int i = 0;
+        int j = nums.length;
+        int pos = -1;
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            if (nums[mid] == target) {
+                pos = mid;
+                break;
+            }
+            if (nums[mid] < target) {
+                i = mid + 1;
+            }
+            if (nums[mid] > target) {
+                j = mid - 1;
+            }
+        }
+        return pos != -1 ? pos : i;
     }
 }
